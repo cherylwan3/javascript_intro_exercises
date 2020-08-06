4bb0d2802dfafab2e6c3d6004548da293e9fe0a7
Cheryl Wan <cherylwan3@gmail.com>
2020-08-05 21:55:55 -0700

    Add preperation exercises

---
+++ preparations_exercises/foo.js
@@ -0,0 +1,38 @@
+let foo = 'bar';
+console.log(foo);
+foo;
+
+/* #5: 
+a) substring
+-String; Prototype method (instance)
+
+b) create 
+-object; static method
+
+c) fromCharCode 
+-String; static method 
+
+d) slice = 
+-String; prototype or instance method
+-Array; prototype
+
+#6:
+index
+lazyDog
+operand2
+ 
+#7
+index
+lazyDog
+operand2
+CatName
+
+#8
+CatName
+BIG_NUMBER
+
+#9:
+snake_case
+quick_Fox
+1stCharacter
+*/
