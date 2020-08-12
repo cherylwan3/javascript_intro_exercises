31f2ef6f6aafd932a062291d5abd4d2a9a982ffa
Cheryl Wan <cherylwan3@gmail.com>
2020-08-12 14:44:54 -0700

    Modifying exercise10 in Ch7:Array

--- ch7_arrays/exercise10.js
+++ ch7_arrays/exercise10.js
@@ -4,3 +4,22 @@
 let arr = [["test", "hello", "world"], ["example", 6, "mem", null], [4, 8, 12]];
 
 console.log(arr[1][2]);
+
+// for challenging myself: I wrote a function that logs 'Mem',
+// if it's found inside a double nested array. It returns a new array 
+// that contains all instances of 'mem'. 
+function logMemInDoubleNestedArray(arr) {
+  let newArr = [];
+  arr.forEach(arrElement => {
+    arrElement.forEach(letter => {
+      if (letter === 'mem') {
+        newArr.push(letter);
+      };
+    });
+  });
+  if (newArr[0] === 'mem') {
+    console.log('mem');
+  } 
+  return newArr;
+}
+console.log(logMemInDoubleNestedArray(arr));
